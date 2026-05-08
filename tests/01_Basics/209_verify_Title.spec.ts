2451cb5e793ff19803e7ca5af0636180d310f4f6
Sam2erSharma <sameersharma2892@gmail.com>
2026-05-08 23:30:12 +0530

    Add Playwright Fundamentals - basics and TypeScript tests

---
+++ tests/01_Basics/209_verify_Title.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test'; // {test, expect} are the pre defined functions and are called as fixtures. We can change the name 'test' to anything else.
+
+test('Verify the title of app.vwo.com', async ({ page }) => { // {page} is called a page fixture which is used to interact with the browser and passed as argument.
+    await page.goto('https://app.vwo.com'); // goto method is used to navigate to a URL
+    await expect(page).toHaveTitle("Login - VWO"); // expect is used to make an assertion and to haveTitle method is used to check the title of the page
+});
