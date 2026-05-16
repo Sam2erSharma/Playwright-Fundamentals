95756c91d0f038360d03814e4da6260dc3fc6e1e
Sam2erSharma <sameersharma2892@gmail.com>
2026-05-16 23:45:13 +0530

    Update VWO free trial test: add timeout option and comment out waitForTimeout

--- tests/Projects/03_VWO_Free_Trail_error.spec.ts
+++ tests/Projects/03_VWO_Free_Trail_error.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from "@playwright/test";
 test("Free Trail", async ({ page }) => {
     //  await page.goto("https://app.vwo.com/#login");
     // await page.getByRole('link', { name: 'Start a FREE TRIAL' }).click();
-    // await page.waitForTimeout(50000);
+    // await page.waitForTimeout(5000);
     await page.goto("https://vwo.com/free-trial/?utm_medium=website&utm_source=login-page&utm_campaign=mof_eg_loginpage");
-    //await page.getByRole('textbox', { name: 'Business Email' }).fill("abcd@gmail.com")
+    //await page.getByRole('textbox', { name: 'Business Email' }).fill("abcd@gmail.com", { timeout: 5000 });
     await page.getByRole('textbox', { name: 'Business Email' }).pressSequentially("abcd@gmail.com", { delay: 100 });
-    await page.waitForTimeout(3000);
+    //await page.waitForTimeout(3000);
     const err_msg = page.getByText("gmail.com doesn't look like a business domain. Please use your business email.");
     await expect(err_msg).toBeVisible();
     console.log(await err_msg.textContent());
